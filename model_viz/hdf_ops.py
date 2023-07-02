93685796d5bbaf22d6a86e4eb8a8990a864688a4
Vyom Patel <vyom.patel@usask.ca>
2023-07-02 01:51:47 -0400

    add new getters

--- model_viz/hdf_ops.py
+++ model_viz/hdf_ops.py
@@ -14,7 +14,7 @@ class HDFReader:
 
     def __post_init__(self):
         self.hdf = h5py.File(self.path, self.mode)
-        self.data = self.hdf[self.name]
+        self.__data = self.hdf
 
     def __getitem__(self, key):
         return self.data[key]
@@ -28,15 +28,26 @@ class HDFReader:
     def __del__(self):
         self.hdf.close()
 
+    def get_data(self, group, stock) -> np.ndarray:
+        """Returns stock data as numpy array"""
+        return self.__data[f"{group}/{stock}"]
+
     def get_stock(self, stock) -> np.ndarray:
         """Returns stock data as numpy array"""
-        return self.data["stocks"][stock][:]
+        return self.get_data("stocks", stock)
 
-    def get_stock_names(self) -> List[str]:
-        """Returns list of stock names"""
-        return self.data["stocks"].keys()
+    def get_stock_with_empirical_data(self, stock) -> np.ndarray:
+        """Returns stock data with empirical data as numpy array"""
+        return self.get_data("stocks_with_empirical_data", stock)
 
-    def stock_iterator(self) -> Iterator[np.ndarray]:
+    def get_empirical_data(self) -> List[np.ndarray]:
+        """Returns empirical data as list of numpy arrays"""
+        empirical_data = []
+        for data in self.__data["empirical_data"].keys():
+            empirical_data.append(self.__data[f"empirical_data/{data}"])
+        return empirical_data
+
+    def get_iterator(self, group) -> Iterator[np.ndarray]:
         """Iterates over stocks"""
-        for stock in self.data["stocks"].keys():
-            yield self.get_stock(stock)
+        for stock in self.__data[group].keys():
+            yield self.get_data(group, stock)
