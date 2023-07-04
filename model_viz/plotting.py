57ded159f3e5975690372a0eef13dcc20e0e742c
Vyom Patel <vyom.patel@usask.ca>
2023-07-04 15:02:19 -0400

    add 2dhist plot

--- model_viz/plotting.py
+++ model_viz/plotting.py
@@ -1,5 +1,6 @@
 import plotly.graph_objects as go
 import numpy as np
+import pandas as pd
 
 
 class BasePlotter:
@@ -16,16 +17,32 @@ class BasePlotter:
 class Histogram2D(BasePlotter):
     name = "Histogram2D"
 
-    def __init__(self, data, empirical_data=None):
+    def __init__(self, data: np.ndarray, empirical_data=None):
         super().__init__()
         self.data = data
         self.empirical_data = empirical_data
 
-    def create_plot(self):
-        x = np.arange(self.data.shape[0])
-        y = self.data
-        return go.Histogram2d(
-            x=x,
-            y=y,
-            nbinsx=x.shape[0],
+    def create_plot(self, **kwargs):
+        """Create 2D histogram plot from samples and overlay empirical data if provided.
+
+        Returns:
+            go.Figure: Plotly figure
+        """
+        x = np.tile(np.arange(self.data.shape[1]), self.data.shape[0])
+        y = self.data.flatten()
+        title = kwargs.get("title", "Histogram2D")
+        x_title = kwargs.get("x_title", "Time")
+        y_title = kwargs.get("y_title", "Value")
+
+        return go.Figure(
+            go.Histogram2d(
+                x=x,
+                y=y,
+                colorbar=dict(title="Count", titleside="right"),
+                histfunc="count",
+            )
+        ).update_layout(
+            title=title,
+            xaxis_title=x_title,
+            yaxis_title=y_title,
         )
