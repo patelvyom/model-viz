6cc370f2d9b75f42a3ad77af128f8bd30d2dbc19
Vyom Patel <vyom.patel@usask.ca>
2023-07-04 21:24:17 -0400

    add box plot function

--- model_viz/plotting.py
+++ model_viz/plotting.py
@@ -1,6 +1,7 @@
 import plotly.graph_objects as go
 import numpy as np
-import pandas as pd
+
+# TODO: Add a ConfigParser to read in config file
 
 
 class BasePlotter:
@@ -10,7 +11,7 @@ class BasePlotter:
         self.data = None
         self.empirical_data = None
 
-    def create_plot(self, stock):
+    def create_plot(self, **kwargs):
         raise NotImplementedError
 
 
@@ -52,6 +53,52 @@ class Histogram2D(BasePlotter):
                 go.Scatter(
                     x=np.arange(self.empirical_data.shape[0]),
                     y=self.empirical_data,
+                    mode="markers",
+                )
+            )
+
+        return fig
+
+
+class BoxPlotOverTime(BasePlotter):
+    name = "BoxPlotOverTime"
+
+    def __init__(self, data: np.ndarray, empirical_data=None):
+        super().__init__()
+        self.data = data
+        self.empirical_data = empirical_data
+
+    def create_plot(self, **kwargs):
+        """Create a box plot over time overlayed with empirical data if provided.
+
+        Returns:
+            go.Figure: Plotly figure
+        """
+
+        title = kwargs.get("title", "BoxPlotOverTime")
+        x_title = kwargs.get("x_title", "Time")
+        y_title = kwargs.get("y_title", "Value")
+
+        fig = go.Figure(
+            go.Box(
+                x=np.tile(np.arange(self.data.shape[1]), self.data.shape[0]),
+                y=self.data.flatten(),
+                boxpoints=False,
+                showlegend=False,
+            )
+        ).update_layout(
+            title=title,
+            xaxis_title=x_title,
+            yaxis_title=y_title,
+        )
+
+        if self.empirical_data is not None:
+            fig.add_trace(
+                go.Scatter(
+                    x=np.arange(self.empirical_data.shape[0]),
+                    y=self.empirical_data,
+                    mode="markers",
+                    showlegend=False,
                 )
             )
 
