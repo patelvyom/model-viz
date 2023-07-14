5c5772a47e8db0103a0612c3d3132d9205ce19de
Vyom Patel <vyom.patel@usask.ca>
2023-07-14 10:43:14 -0600

    add export plots option

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import model_viz
 import dash
 from dash import Dash, html, dcc, Input, Output
 from itertools import chain
-
+import functools
 
 graph_types = [
     {"label": "2D-Histogram", "value": "2d_hist"},
@@ -12,11 +12,11 @@ graph_types = [
 app = dash.Dash(__name__)
 
 
-def generate_figs(hdf_path, graph_type):
+def generate_plots(hdf_path, graph_type):
     reader = model_viz.hdf_ops.HDFReader("reader", hdf_path)
     stocks = reader.get_iterator("stocks")
     stocks_with_empirical_data = reader.get_iterator("stocks_with_empirical_data")
-    figs = []
+    plots = []
     if graph_type == "2d_hist":
         plotter = model_viz.plotting.Histogram2D
     elif graph_type == "boxplot_over_time":
@@ -26,19 +26,18 @@ def generate_figs(hdf_path, graph_type):
 
     for stock in chain(stocks_with_empirical_data, stocks):
         title = stock.name.split("/")[-1]
-        print(f"Creating fig for {title}")
+        print(f"Creating plot for {title}")
         data = stock["model_values"][:]
         empirical_data = (
             stock["empirical_values"][:].flatten()
             if "empirical_values" in stock
             else None
         )
-        fig = plotter(data=data, empirical_data=empirical_data).create_plot(
-            title=title, x_title="Time", y_title="Value"
-        )
-        figs.append(html.Div([dcc.Graph(figure=fig)]))
+        plot = plotter(data=data, empirical_data=empirical_data)
+        plot.create_plot(title=title, x_title="Time", y_title="Value")
+        plots.append(plot)
 
-    return figs
+    return plots
 
 
 def main(argv):
@@ -48,14 +47,28 @@ def main(argv):
             dcc.Dropdown(
                 options=graph_types, placeholder="Select Graph Type", id="graph_type"
             ),
+            html.Button("Export", id="export"),
             html.Div(id="figures"),
         ]
     )
 
     @app.callback(Output("figures", "children"), Input("graph_type", "value"))
+    @functools.lru_cache(maxsize=32)
     def update_graph(graph_type):
         if graph_type is not None:
-            return generate_figs(argv[0], graph_type)
+            return [
+                dcc.Graph(figure=plot.fig)
+                for plot in generate_plots(argv[0], graph_type)
+            ]
+
+    @app.callback(
+        Output("export", "n_clicks"),
+        Input("graph_type", "value"),
+        Input("export", "n_clicks"),
+    )
+    def export_plots(graph_type, n_clicks):
+        if graph_type is not None and n_clicks is not None:
+            [plot.export_plot() for plot in generate_plots(argv[0], graph_type)]
 
     app.run_server(debug=True)
 
