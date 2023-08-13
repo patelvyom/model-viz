b0a6f3d62c2f577121428b1ebe653e0a7a361087
Vyom Patel <vyom.patel@usask.ca>
2023-08-12 18:43:22 -0600

    add optional date labels

--- model_viz/plotting.py
+++ model_viz/plotting.py
@@ -12,9 +12,9 @@ class BasePlotter:
     fig = None
     title = None
 
-    def __init__(self):
-        self.data = None
-        self.overlay_data = None
+    def __init__(self, data=None, overlay_data=None):
+        self.data = data
+        self.overlay_data = overlay_data
 
     def create_plot(self, **kwargs):
         raise NotImplementedError
@@ -32,14 +32,28 @@ class BasePlotter:
         )
         return path
 
+    def update_x_ticks(self, x_tick_vals):
+        if config.Plotter.is_xlabel_date:
+            tick_interval = max(
+                1, x_tick_vals.shape[0] // config.Plotter.desired_tick_labels
+            )
+            x_tick_text = pd.to_datetime(config.Plotter.day_zero) + pd.to_timedelta(
+                x_tick_vals, unit="D"
+            )
+            x_tick_text = x_tick_text.strftime("%Y-%m-%d")
+            self.fig.update_xaxes(
+                tickangle=-45,
+                tickmode="array",
+                tickvals=x_tick_vals[::tick_interval],
+                ticktext=x_tick_text[::tick_interval],
+            )
+
 
 class Histogram2D(BasePlotter):
     name = "Histogram2D"
 
     def __init__(self, data: np.ndarray, overlay_data=None):
-        super().__init__()
-        self.data = data
-        self.overlay_data = overlay_data
+        super().__init__(data, overlay_data)
 
     def create_plot(self, **kwargs) -> go.Figure:
         """Create 2D histogram plot from samples and overlay empirical data if provided.
@@ -88,6 +102,7 @@ class Histogram2D(BasePlotter):
                 )
             )
         self.fig = fig
+        self.update_x_ticks(np.arange(self.data.shape[1]))
         return fig
 
 
@@ -95,9 +110,7 @@ class BoxPlotOverTime(BasePlotter):
     name = "BoxPlotOverTime"
 
     def __init__(self, data: np.ndarray, overlay_data=None):
-        super().__init__()
-        self.data = data
-        self.overlay_data = overlay_data
+        super().__init__(data, overlay_data)
 
     def _pre_compute_boxplot_stats(self) -> dict[str, np.ndarray]:
         """Compute boxplot aggregation statistics over time.
@@ -163,6 +176,7 @@ class BoxPlotOverTime(BasePlotter):
                 )
             )
         self.fig = fig
+        self.update_x_ticks(np.arange(self.data.shape[1]))
         return fig
 
 
@@ -172,8 +186,7 @@ class Histogram(BasePlotter):
     """
 
     def __init__(self, data: np.ndarray):
-        super().__init__()
-        self.data = data
+        super().__init__(data)
 
     def create_plot(self, **kwargs) -> go.Figure:
         """Create a histogram plot.
@@ -183,9 +196,10 @@ class Histogram(BasePlotter):
         """
         x_title = kwargs.get("x_title", config.Histogram.x_title)
         y_title = kwargs.get("y_title", config.Histogram.y_title)
-        fig = go.Figure(go.Histogram(y=self.data)).update_layout(  # Flipping axes
+        fig = go.Figure(
+            go.Histogram(y=self.data, opacity=0.9)
+        ).update_layout(  # Flipping axes
             xaxis_title=x_title, yaxis_title=y_title
         )
         self.fig = fig
-
         return fig
