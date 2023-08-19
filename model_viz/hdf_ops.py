26eb10d321a75f644e12fb4e61622d9c52514f2c
Vyom Patel <vyom.patel@usask.ca>
2023-08-19 17:33:39 -0600

    new hdf5 func

--- model_viz/hdf_ops.py
+++ model_viz/hdf_ops.py
@@ -27,7 +27,7 @@ class HDFReader:
         """Iterates over items in a group"""
         return [self.__data[f"{group}/{item}"] for item in self.__data[group]]
 
-    def get_groups(self) -> dict[str, list[h5py.Group]]:
+    def get_all_groups(self) -> dict[str, list[h5py.Group]]:
         """
         Return iterators of "plotting_groups" for all groups
         """
@@ -36,3 +36,17 @@ class HDFReader:
             groups[group] = self._get_group_items(group)
 
         return groups
+
+    def get_group(self, root_group: str, sub_group: list[str]):
+        """
+        Return group at the lowest level of hierarchy (determined by last element of `sub_group`)
+
+        Args:
+            root_group: Root group to start searching from
+            sub_group: List of groups to traverse
+        """
+        group = self.__data[root_group]
+        sub_group = group.get("/".join(sub_group), None)
+        if sub_group is None:
+            raise ValueError(f"Group {sub_group} not found in {root_group}")
+        return sub_group
